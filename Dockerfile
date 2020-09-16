280e3b4cf0e4f6bfa7b5b70079e69ca4fe0ef90e
vgrimmeisen <vincent.grimmeisen@redguard.ch>
2020-09-16 17:43:14 +0200

    Additional setup

--- Dockerfile
+++ Dockerfile
@@ -1,23 +1,33 @@
-FROM golang:latest
+FROM kalilinux/kali-rolling:latest
 RUN apt update
-RUN GO111MODULE=auto go get -u -v github.com/projectdiscovery/subfinder/cmd/subfinder
-RUN GO111MODULE=auto go get -v github.com/OWASP/Amass/v3/...
-RUN go get -u github.com/tomnomnom/httprobe
-
-# Setup Python
-RUN apt install -y build-essential zlib1g-dev libncurses5-dev libgdbm-dev libnss3-dev libssl-dev libreadline-dev libffi-dev wget curl
+# Install essentials
+RUN apt install -y build-essential zlib1g-dev libncurses5-dev libgdbm-dev libnss3-dev libssl-dev libreadline-dev libffi-dev wget curl git
+# Install go 
+RUN wget https://dl.google.com/go/go1.13.4.linux-amd64.tar.gz
+RUN tar -C /usr/local -xzf go1.13.4.linux-amd64.tar.gz
+ENV PATH="${PATH}:/usr/local/go/bin"
+RUN go version
+# Install Python
 RUN curl -O https://www.python.org/ftp/python/3.8.0/Python-3.8.0.tar.xz
 RUN tar -xf Python-3.8.0.tar.xz  
 RUN cd Python-3.8.0 &&\
     ./configure --enable-optimizations &&\
     make &&\
     make altinstall
-
+# Python dependencies telegram
 RUN apt install python3-pip -y
 RUN pip3 install python-telegram-bot --upgrade
+# Install amass
+RUN apt-get install amass -y
+# Install subfinder
+RUN GO111MODULE=auto go get -u -v github.com/projectdiscovery/subfinder/cmd/subfinder
 # Setup Bash script
 COPY main.sh /root/main.sh
 RUN chmod +x /root/main.sh
 COPY message.py /code/message.py
 COPY telegram.token /code/telegram.token
+
+
+# Create output folder inside the container
+RUN mkdir /data
 ENTRYPOINT [ "../root/main.sh" ]
--- main.sh
+++ main.sh
@@ -2,28 +2,37 @@
 DOMAIN=$1
 BASE_DIR="/root/$DOMAIN"
 TOOLS_DIR='/root/tools'
-
+echo $DOMAIN
 function notify {
-    python3 /code/message.py "$1"
+    python3 /code/message.py "$1" 
 }
 
 function amass-exec {
-    #amass enum --passive -d "$DOMAIN" -o "$baseDir/amass-$DOMAIN.txt" 
-    amass --version
+    amass enum --passive -d "$DOMAIN" -o "$BASE_DIR/amass-$DOMAIN.txt"
+    num_domains=$(cat "$BASE_DIR/amass-$DOMAIN.txt" | wc -l)
+    notify "amass - $DOMAIN - $num_domains"
+}
+
+function subfinder-exec {
+    subfinder -d $DOMAIN -nC -o "$BASE_DIR/subfinder-$DOMAIN.txt" -silent -t 64 -timeout 10 -all &>> "$BASE_DIR/debug.txt"
 }
 
 function httpx-exec {
-    httpx -silent -threads 64 -no-color -l "$baseDir/amass-$DOMAIN.txt" -title -content-length -web-server -status-code -ports 80,81,443,4000,4001,4002,4433,8008,8080,8443,8888  -o "$baseDir/httpx-$DOMAIN.txt"
+    httpx -silent -threads 64 -no-color -l "$BASE_DIR/amass-$DOMAIN.txt" -title -content-length -web-server -status-code -ports 80,81,443,4000,4001,4002,4433,8008,8080,8443,8888  -o "$BASE_DIR/httpx-$DOMAIN.txt"
 }
 
 function main {
-    amass-exec
+    #amass-exec
+    subfinder-exec
 }
-start_time=`date +%s`
-mkdir "$BASEDIR"
-main 
-end=`date +%s`
-runtime=$((end-start))
-notify "Finished $DOMAIN - $runtime"
+
+mkdir "$BASE_DIR"
+{ time main ; } 2> time.txt
+t=$(cat time.txt | grep real | cut -d " " -f2)
+cp -r $BASE_DIR /data >> "$BASE_DIR/debug.txt"
+ls -lah >> "$BASE_DIR/debug.txt"
+DEBUG_CONTENT=$(cat "$BASE_DIR/debug.txt")
+notify "Debug: $DEBUG_CONTENT"
+notify "Enum for $DOMAIN - $t"
 
 
